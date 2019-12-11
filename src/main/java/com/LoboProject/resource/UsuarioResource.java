bfc73b7d26a6d4ea99d3d2f2f4d368a43d5cfab1
Sandro <sandro.adriano001@gmail.com>
2019-12-11 11:32:50 -0300

    adicionado

--- src/main/java/com/LoboProject/resource/UsuarioResource.java
+++ src/main/java/com/LoboProject/resource/UsuarioResource.java
@@ -2,7 +2,6 @@ package com.LoboProject.resource;
 
 import java.util.List;
 import java.util.Optional;
-import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
