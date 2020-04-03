72a3316b1bd6ebc2ce7a52d17b8e7faf4bde1e9b
Sandro <sandro.adriano001@gmail.com>
2020-04-03 12:26:22 -0300

    sk

--- src/main/java/com/LoboProject/service/PedidoService.java
+++ src/main/java/com/LoboProject/service/PedidoService.java
@@ -349,7 +349,7 @@ public class PedidoService {
 		if(op == 0) {
 			produto.setQuantidadeMax(-((produto.getQuantidadeMax()) - produto.getQuantidadeMax() + l) - produto.getQuantidadeMin());
 		}else {
-			produto.setQuantidadeMax(produto.getQuantidadeMax() - l - produto.getQuantidadeAtual()*2);
+			produto.setQuantidadeMax(produto.getQuantidadeMax() - l - produto.getQuantidadeAtual());
 		}
 		return produto;
 	}
